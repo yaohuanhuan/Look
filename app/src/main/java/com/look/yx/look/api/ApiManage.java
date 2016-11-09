1bef46f5d9e6c833a05a1dcc4a4c2f198aa43386
yaohuanhuan <1107129170@qq.com>
2016-11-09 19:11:14 +0800

    初步完成Retrofit和接口调用

---
+++ app/src/main/java/com/look/yx/look/api/ApiManage.java
@@ -0,0 +1,64 @@
+package com.look.yx.look.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by yx on 2016/11/8.
+ */
+
+public class ApiManage {
+
+    //私有构造方法
+    private ApiManage(){}
+    //ApiManage实例
+    public static ApiManage apiManage;
+    //ZhihuApi引用
+    public ZhihuApi zhihuApi;
+    //OkHttp打印日志类，可以打印http请求信息
+    public static HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    //OkHttpClient实例
+    public static OkHttpClient client = new OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build();
+
+    private Object zhihuMonitor = new Object();
+
+    /**
+     * 拿到ApiManage实例
+     * @return
+     */
+    public static ApiManage getInstence(){
+        if (apiManage == null){
+            synchronized (ApiManage.class){
+                if (apiManage == null){
+                    apiManage = new ApiManage();
+                }
+            }
+        }
+        return apiManage;
+    }
+
+    /**
+     * 拿到ZhihuApi引用
+     * @return
+     */
+    public ZhihuApi getZhihuApiService(){
+        if (zhihuApi == null){
+            synchronized (zhihuMonitor){
+                if (zhihuApi == null){
+                    zhihuApi = new Retrofit.Builder()
+                            .baseUrl("http://news-at.zhihu.com")
+                            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .client(client)
+                            .build().create(ZhihuApi.class);
+                }
+            }
+        }
+        return zhihuApi;
+    }
+}
---
+++ app/src/main/java/com/look/yx/look/api/ZhihuApi.java
@@ -0,0 +1,23 @@
+package com.look.yx.look.api;
+
+
+import com.look.yx.look.bean.zhihu.ZhihuDaily;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+import rx.Observer;
+
+/**
+ * Created by yx on 2016/11/9.
+ */
+
+public interface ZhihuApi {
+
+    @GET("/api/4/news/latest")
+    Observer<ZhihuDaily> getLastDaily();
+
+    @GET("/api/4/news/before/{date}")
+    Observable<ZhihuDaily> getTheDaily(@Path("date") String date);
+
+}
