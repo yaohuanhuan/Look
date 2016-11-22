3b0296a71827d66007aa29497ac80c319bd274d3
yaohuanhuan <1107129170@qq.com>
2016-11-22 21:07:27 +0800

    各种自定义View和资源文件

--- app/src/main/java/com/look/yx/look/activity/ZhihuDescribeActivity.java
+++ app/src/main/java/com/look/yx/look/activity/ZhihuDescribeActivity.java
@@ -1,11 +1,44 @@
 package com.look.yx.look.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.transition.Transition;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
 
+
+import com.look.yx.look.R;
 import com.look.yx.look.bean.zhihu.ZhihuStory;
+import com.look.yx.look.presenter.IZhihuStoryPresenter;
+import com.look.yx.look.presenter.implPresenter.ZhihuStoryPresenterImpl;
 import com.look.yx.look.presenter.implView.IZhihuStory;
+import com.look.yx.look.util.AnimUtils;
+import com.look.yx.look.widget.ElasticDragDismissFrameLayout;
+import com.look.yx.look.widget.ParallaxScrimageView;
+import com.look.yx.look.widget.TranslateYTextView;
+
+import java.lang.reflect.InvocationTargetException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by yx on 2016/11/21.
@@ -13,19 +46,274 @@ import com.look.yx.look.presenter.implView.IZhihuStory;
 
 public class ZhihuDescribeActivity extends AppCompatActivity implements IZhihuStory {
 
+    @BindView(R.id.shot)
+    ParallaxScrimageView mShot;
+    @BindView(R.id.title)
+    TranslateYTextView mTranslateYTextView;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.wv_zhihu)
+    WebView wvZhihu;
+    @BindView(R.id.nest)
+    NestedScrollView mNest;
+    @BindView(R.id.container)
+    FrameLayout container;
+    @BindView(R.id.draggable_frame)
+    ElasticDragDismissFrameLayout mDraggableFrame;
+
+    boolean isEmpty;
+    String mBody;
+    String[] scc;
+    String mImageUrl;
+
+    int[] mDeviceInfo;
+    int width;
+    int heigh;
+    private Transition.TransitionListener zhihuReturnHomeListener;
+    private NestedScrollView.OnScrollChangeListener scrollListener;
+
+    private String id;
+    private String title;
+    private String url;
+    private IZhihuStoryPresenter mIZhihuStoryPresenter;
+    private ElasticDragDismissFrameLayout.SystemChromeFader chromeFader;
+    private Handler mHandler=new Handler();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.zhihudescribe);
+        ButterKnife.bind(this);
     }
+
+    private void initlistenr() {
+        zhihuReturnHomeListener =
+                new AnimUtils.TransitionListenerAdapter() {
+                    @Override
+                    public void onTransitionStart(Transition transition) {
+                        super.onTransitionStart(transition);
+                        // hide the fab as for some reason it jumps position??  TODO work out why
+                        mToolbar.animate()
+                                .alpha(0f)
+                                .setDuration(100)
+                                .setInterpolator(new AccelerateInterpolator());
+                        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+                            mShot.setElevation(1f);
+                            mToolbar.setElevation(0f);
+                        }
+                        mNest.animate()
+                                .alpha(0f)
+                                .setDuration(50)
+                                .setInterpolator(new AccelerateInterpolator());
+                    }
+                };
+        scrollListener = new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (oldScrollY<168){
+                    mShot.setOffset(-oldScrollY);
+                    mTranslateYTextView.setOffset(-oldScrollY);
+                }
+
+            }
+        };
+    }
+
+    private void initData() {
+        id = getIntent().getStringExtra("id");
+        title = getIntent().getStringExtra("title");
+        mImageUrl = getIntent().getStringExtra("image");
+        mIZhihuStoryPresenter = new ZhihuStoryPresenterImpl(this);
+        mNest.setOnScrollChangeListener(scrollListener);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            postponeEnterTransition();
+            mShot.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    mShot.getViewTreeObserver().removeOnPreDrawListener(this);
+                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+                        startPostponedEnterTransition();
+                    }
+                    return true;
+                }
+            });
+        }
+
+
+    }
+
+    private void initView() {
+        mToolbar.setTitleMargin(20,20,0,10);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        mToolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mNest.smoothScrollTo(0,0);
+            }
+        });
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expandImageAndFinish();
+            }
+        });
+        mTranslateYTextView.setText(title);
+
+        WebSettings settings = wvZhihu.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        settings.setLoadWithOverviewMode(true);
+        settings.setBuiltInZoomControls(true);
+        //settings.setUseWideViewPort(true);造成文字太小
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAppCachePath(getCacheDir().getAbsolutePath() + "/webViewCache");
+        settings.setAppCacheEnabled(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        wvZhihu.setWebChromeClient(new WebChromeClient());
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mDraggableFrame.addListener(chromeFader);
+        try {
+            wvZhihu.getClass().getMethod("onResume").invoke(wvZhihu, (Object[]) null);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDraggableFrame.removeListener(chromeFader);
+        try {
+            wvZhihu.getClass().getMethod("onPause").invoke(wvZhihu, (Object[]) null);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+
+            getWindow().getSharedElementReturnTransition().removeListener(zhihuReturnHomeListener);
+        }
+        //webview内存泄露
+        if (wvZhihu != null) {
+            ((ViewGroup) wvZhihu.getParent()).removeView(wvZhihu);
+            wvZhihu.destroy();
+            wvZhihu = null;
+        }
+        mIZhihuStoryPresenter.unsubcrible();
+        super.onDestroy();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        expandImageAndFinish();
+
+    }
+
+    private void getData() {
+        mIZhihuStoryPresenter.getZhihuStory(id);
+
+    }
+
+    @OnClick(R.id.shot)
+    public void onClick() {
+        mNest.smoothScrollTo(0, 0);
+
+    }
+
     @Override
     public void showError(String error) {
-
+        Snackbar.make(wvZhihu, getString(R.string.snack_infor), Snackbar.LENGTH_INDEFINITE).setAction("重试", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        }).show();
     }
 
     @Override
     public void showZhihuStory(ZhihuStory zhihuStory) {
 
     }
+
+    private void expandImageAndFinish() {
+        if (mShot.getOffset() != 0f) {
+            Animator expandImage = ObjectAnimator.ofFloat(mShot, ParallaxScrimageView.OFFSET,
+                    0f);
+            expandImage.setDuration(80);
+            expandImage.setInterpolator(new AccelerateInterpolator());
+            expandImage.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+
+                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+                        finishAfterTransition();
+                    }else {
+                        finish();
+                    }
+                }
+            });
+            expandImage.start();
+        } else {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+                finishAfterTransition();
+            }else {
+                finish();
+            }
+        }
+    }
+
+    private void enterAnimation() {
+        float offSet = mToolbar.getHeight();
+        LinearInterpolator interpolator=new LinearInterpolator();
+        viewEnterAnimation(mShot, offSet, interpolator);
+        viewEnterAnimationNest(mNest,0f,interpolator);
+
+    }
+
+    private void viewEnterAnimation(View view, float offset, Interpolator interp) {
+        view.setTranslationY(-offset);
+        view.setAlpha(0f);
+        view.animate()
+                .translationY(0f)
+                .alpha(1f)
+                .setDuration(500L)
+                .setInterpolator(interp)
+                .setListener(null)
+                .start();
+    }
+    private void viewEnterAnimationNest(View view, float offset, Interpolator interp) {
+        view.setTranslationY(-offset);
+        view.setAlpha(0.3f);
+        view.animate()
+                .translationY(0f)
+                .alpha(1f)
+                .setDuration(500L)
+                .setInterpolator(interp)
+                .setListener(null)
+                .start();
+    }
+
+
 
 
 }
